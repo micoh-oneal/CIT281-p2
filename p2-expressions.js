32066a6c48b422e51b56eb28feca493bbe22b17e
micoh-oneal <105960232+micoh-oneal@users.noreply.github.com>
2022-06-03 20:38:10 -0700

    Add files via upload

---
+++ p2-expressions.js
@@ -0,0 +1,40 @@
+/*
+    CIT 281 Project 2
+    Name: Michael O'Neal
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+const getRandomInteger = function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+let result = "";
+
+let lengthOfOutputString = getRandomInteger(5, 26);
+
+// Returns a single, random, lowercase letter
+const getRandomLetter = function getRandomLetter() {
+    randomLetter = alphabet[getRandomInteger(0,alphabet.length)];
+    return randomLetter;
+}
+
+// Returns a random length string with random lowercase letters
+const getRandomString = function getRandomString(minLength, maxLength) {
+    for (let j = 0; j < getRandomInteger(10, 20); j++) {
+        result += getRandomLetter();
+    }
+    return result
+}
+
+//Returns a string in ascending order
+const getSortedString = function getSortedString(string) {
+    return string.split('').sort().join(''); 
+}
+
+for (let i = 0; i < lengthOfOutputString; i++) {
+    result += getRandomLetter();
+}
+
+getRandomString();
